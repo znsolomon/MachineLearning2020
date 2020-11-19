d67d3725d472442d4b63736fe04fc6c598ddb0f1
znsolomon <znsolomon@gmail.com>
2020-11-19 15:10:05 +0000

    Reducing data to run ML algorithms faster

--- main.py
+++ main.py
@@ -1,9 +1,86 @@
 import mnist_reader
+import numpy as np
+import sklearn
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+
+def divide_data(data, labels, size):
+    #Cut down size of data whilst retaining proportions of classes
+    sortedData = np.empty([10, 6000, 784])
+    sortedLabels = np.empty([10, 6000])
+    counters = np.zeros(10).astype(int)
+    #Sort data into labels
+    for i in range(len(data)):
+        label = labels[i]
+        sortedData[label, counters[label]] = data[i]
+        sortedLabels[label, counters[label]] = labels[i]
+        counters[label] += 1
+
+
+
+
+def dbscan(trainSet, trainLabels):
+
+    X = trainSet
+    labels_true = trainLabels
+
+    X = StandardScaler().fit_transform(X)
+
+    # Compute DBSCAN
+    db = DBSCAN(eps=0.3, min_samples=10).fit(X)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+
+    # Number of clusters in labels, ignoring noise if present.
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+
+    print('Estimated number of clusters: %d' % n_clusters_)
+    print('Estimated number of noise points: %d' % n_noise_)
+    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+    print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+    print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+    print("Adjusted Rand Index: %0.3f"
+          % metrics.adjusted_rand_score(labels_true, labels))
+    print("Adjusted Mutual Information: %0.3f"
+          % metrics.adjusted_mutual_info_score(labels_true, labels))
+    print("Silhouette Coefficient: %0.3f"
+          % metrics.silhouette_score(X, labels))
+
+    # Black removed and is used for noise instead.
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each)
+              for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = [0, 0, 0, 1]
+
+        class_member_mask = (labels == k)
+
+        xy = X[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=14)
+
+        xy = X[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
+                 markeredgecolor='k', markersize=6)
+
+    plt.title('Estimated number of clusters: %d' % n_clusters_)
+    plt.show()
 
 
 if __name__ == '__main__':
 
     X_train, y_train = mnist_reader.load_mnist('data/fashion', kind='train')
     X_test, y_test = mnist_reader.load_mnist('data/fashion', kind='t10k')
+
+    divide_data(X_train, y_train, 30000)
+
+    #dbscan(X_train, y_train)
 
 
