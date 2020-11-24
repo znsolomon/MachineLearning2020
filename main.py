958c8d5a65dcc458fe04cc8a01c65d8db9d8c80d
znsolomon <znsolomon@gmail.com>
2020-11-24 14:30:15 +0000

    Implemented three ML methods

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import mnist_reader
 import numpy as np
-import sklearn
-from sklearn.cluster import DBSCAN
-from sklearn import metrics
 from sklearn import tree
-from sklearn.datasets import make_blobs
-from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
 import matplotlib.pyplot as plt
 
 
@@ -35,70 +32,34 @@ def divide_data(data, labels, size):
 
 
 def decision_tree(trainSet, trainLabels, testSet, testLables):
-    x = trainSet
-    y = trainLabels
     clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(x, y)
+    clf = clf.fit(trainSet, trainLabels)
     predictions = clf.predict(testSet)
+    return get_accuracy(predictions, testLables)
+
+
+def nearest_neighbors(trainSet, trainLables, testSet, testLables, k):
+    neigh = KNeighborsClassifier(n_neighbors=k)
+    neigh.fit(trainSet, trainLables)
+    predictions = neigh.predict(testSet)
+    return get_accuracy(predictions, testLables)
+
+
+def gaussian_bayes(trainSet, trainLables, testSet, testLables):
+    bayes = GaussianNB()
+    bayes.fit(trainSet, trainLables)
+    predictions = bayes.predict(testSet)
+    return get_accuracy(predictions, testLables)
+
+
+def get_accuracy(predict, trueLables):
     # Check % of correct predictions
     accuracy = 0
-    for i in range(len(predictions)):
-        if predictions[i] == testLables[i]:
+    for i in range(len(predict)):
+        if predict[i] == trueLables[i]:
             accuracy += 1
-    accuracy = accuracy / len(predictions)
+    accuracy = accuracy / len(predict)
     return accuracy
-
-
-def dbscan(trainSet, trainLabels):
-
-    X = trainSet
-    labels_true = trainLabels
-
-    X = StandardScaler().fit_transform(X)
-
-    # Compute DBSCAN
-    db = DBSCAN(eps=0.5, min_samples=5).fit(X)
-    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-    core_samples_mask[db.core_sample_indices_] = True
-    labels = db.labels_
-
-    # Number of clusters in labels, ignoring noise if present.
-    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-    n_noise_ = list(labels).count(-1)
-
-    print('Estimated number of clusters: %d' % n_clusters_)
-    print('Estimated number of noise points: %d' % n_noise_)
-    print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-    print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-    print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-    print("Adjusted Rand Index: %0.3f"
-          % metrics.adjusted_rand_score(labels_true, labels))
-    print("Adjusted Mutual Information: %0.3f"
-          % metrics.adjusted_mutual_info_score(labels_true, labels))
-    #print("Silhouette Coefficient: %0.3f"
-    #      % metrics.silhouette_score(X, labels))
-
-    # Black removed and is used for noise instead.
-    unique_labels = set(labels)
-    colors = [plt.cm.Spectral(each)
-              for each in np.linspace(0, 1, len(unique_labels))]
-    for k, col in zip(unique_labels, colors):
-        if k == -1:
-            # Black used for noise.
-            col = [0, 0, 0, 1]
-
-        class_member_mask = (labels == k)
-
-        xy = X[class_member_mask & core_samples_mask]
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-                 markeredgecolor='k', markersize=14)
-
-        xy = X[class_member_mask & ~core_samples_mask]
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-                 markeredgecolor='k', markersize=6)
-
-    plt.title('Estimated number of clusters: %d' % n_clusters_)
-    plt.show()
 
 
 if __name__ == '__main__':
@@ -108,5 +69,5 @@ if __name__ == '__main__':
 
     X_reduced, y_reduced = divide_data(X_train, y_train, 10000)
 
-    print(decision_tree(X_reduced, y_reduced, X_test, y_test))
+    print(gaussian_bayes(X_reduced, y_reduced, X_test, y_test))
 
